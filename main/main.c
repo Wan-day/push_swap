a960b5ed75f433b5239625444c7954221b3c323d
WanDay <boldinov.dima@gmail.com>
2026-09-10 01:32:09 +0200

    Fixed up the main

--- main/main.c
+++ main/main.c
@@ -14,28 +14,36 @@ int	main(int argc, char **argv)
 	t_stack     *a;         // Pointer to stack A (the linked list, head node)
 	t_stack     *b;         // Pointer to stack B, starts as NULL (empty)
 	
+	// Zero the structs so that the values inside are accessible.
 	ft_bzero(&bench, sizeof(bench));
+	ft_bzero(&opts, sizeof(opts));
+
 	// Parses the arguments and extracts options if any are present. 
 	// Sets strategy for opts and bench.
     tokens = extract_options(argc, argv, &opts, &remaining, &bench); 
 
 	// Parses and validates the number tokens, calculates disorder and adds it to bench.
+	// DONT FORGET TO FREE TOKENS INSIDE THE FUNCTION!!!!
     nums = parse_and_validate_tokens(tokens, remaining, &count, &bench); 
 	if (!nums)
 		return (put_error(), 1);
 
 	// Builds the main stack from the number tokens.
-	a = build_stack(nums, count); // first arg = top of stack, per subject
+	a = build_stack(nums, count); // First arg = top of stack, per subject
 	free(nums);
 	b = NULL;
 
 	// Checks if the stack is sorted and if not - calls the sorting funtcion. 
-	// Adds op count to bench.
+	// Adds operations count to bench.
 	if (!is_sorted(a))
-		push_swap(&a, &b, opts.strategy, &bench);
+		sort_stack(&a, &b, opts.strategy, &bench);
 
-	// Prints benchmark if asked for by the argument.
+	// Prints benchmark if asked for by the option.
 	if (opts.bench)
 		print_benchmark(bench);
+
+	// DONT FORGET TO FREE BOTH STACKS.
+	free_stack(&a);
+	free_stack(&b);
     return (0);
 }
