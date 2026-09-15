b0617dff444d9df1cfb44f8eb66c269532837a19
PixelDuk <dolgoon.gankhuyag@gmail.com>
2026-09-15 13:39:22 +0200

    Implementation of operations

--- main/push.c
+++ main/push.c
@@ -1,52 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   operations_1.c                                     :+:      :+:    :+:   */
+/*   push.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: duk <duk@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/09/13 12:29:36 by duk               #+#    #+#             */
-/*   Updated: 2026/09/15 13:30:56 by duk              ###   ########.fr       */
+/*   Created: 2026/09/15 13:33:55 by duk               #+#    #+#             */
+/*   Updated: 2026/09/15 13:36:10 by duk              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-void	sa(t_stack **a)
-{
-	t_stack	*node1;
-	t_stack	*node2;
-	int		tmp;
-
-	if ((*a) == NULL || (*a)->next == (*a))
-		return ;
-	node1 = *a;
-	node2 = (*a)->next;
-	tmp = node1->num;
-	node1->num = node2->num;
-	node2->num = tmp;
-}
-
-void	sb(t_stack **b)
-{
-	t_stack	*node1;
-	t_stack	*node2;
-	int		tmp;
-
-	if ((*b) == NULL || (*b)->next == (*b))
-		return ;
-	node1 = *b;
-	node2 = (*b)->next;
-	tmp = node1->num;
-	node1->num = node2->num;
-	node2->num = tmp;
-}
-
-void	ss(t_stack **a, t_stack **b)
-{
-	sa(a);
-	sb(b);
-}
 
 t_stack	*rmv_top(t_stack **s1)
 {
@@ -109,44 +73,4 @@ void	pb(t_stack **a, t_stack **b)
 	if (tmp == NULL)
 		return ;
 	ins_top (tmp, b);
-}
-
-void	ra(t_stack **a)
-{
-	if ((*a) == NULL || (*a)->next == (*a))
-		return ;
-	(*a) = (*a)->next;
-}
-
-void	rb(t_stack **b)
-{
-	if ((*b) == NULL || (*b)->next == (*b))
-		return ;
-	(*b) = (*b)->next;
-}
-
-void	rr(t_stack **a, t_stack **b)
-{
-	ra(a);
-	rb(b);
-}
-
-void	rra(t_stack **a)
-{
-	if ((*a) == NULL || (*a)->next == (*a))
-		return ;
-	(*a) = (*a)->prev;
-}
-
-void	rrb(t_stack **b)
-{
-	if ((*b) == NULL || (*b)->next == (*b))
-		return ;
-	(*b) = (*b)->prev;
-}
-
-void	rrr(t_stack **a, t_stack **b)
-{
-	rra(a);
-	rrb(b);
 }
